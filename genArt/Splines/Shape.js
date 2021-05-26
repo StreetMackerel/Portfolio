a5e3e0d25879fed7b49bab2f84b5279013e75f4f
StreetMackerel <45179606+StreetMackerel@users.noreply.github.com>
2021-05-26 21:07:59 +0100

    graphic fix

--- genArt/Splines/Shape.js
+++ genArt/Splines/Shape.js
@@ -1,20 +1,2 @@
-class Shape {
-  constructor(vect, vel, accel){
-    this.vect = vect;
-    this.velocity = vel;
-    this.acceleration = accel;
-  }
 
-
- Seek(v){
-  this.velocity.add(this.acceleration);
- // fill(120,181,177);
-  let dVector = p5.Vector.sub(this.vect,v);
-  let mag = dVector.mag();
-  dVector.normalize();
-  dVector.mult(this.velocity);
-  this.vect.sub(dVector);
-  ellipse(this.vect.x,this.vect.y,15,15);
-}
-}
 
